87ba837bbea55406a2727b2e76c6dcf2cb854b43
mathisdelehouzee <mathdel@outlook.com>
2023-09-12 14:44:01 +0200

    debug client.py

--- fl/Client/Client.py
+++ fl/Client/Client.py
@@ -225,7 +225,7 @@ class FlowerClient(fl.client.NumPyClient):
 
   def fit(self, parameters, config):
     self.set_parameters(parameters)
-    train_model(RecurrentAutoencoder, trainloader,valloader, epochs=1)
+    train_model(RecurrentAutoencoder, trainloader,valloader, n_epochs=1)
     return self.get_parameters(config={}), len(trainloader.dataset), {}
 
 #   def evaluate(self, parameters, config):

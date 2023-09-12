0ed75351ca635d080022debbc736c960850a3158
mathisdelehouzee <mathdel@outlook.com>
2023-09-12 14:10:11 +0200

    update

--- fl/Server/start_server.sh
+++ fl/Server/start_server.sh
@@ -1,12 +1,3 @@
-pip install -r requirements.txt
+pip install -r requirements_server.txt
 
-pip install gdown
-if [ ! -f ECG5000 ]; then
-    gdown --id 16MIleqoIr1vYxlGk4GKnGmrsCPuWkkpT
-    sudo apt install unzip
-    mkdir ECG5000
-    unzip -qq ECG5000.zip -d ECG5000
-fi
-
-rm ECG5000.zip
-python3 Client.py
+python3 Server.py
